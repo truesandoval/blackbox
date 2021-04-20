59fb7dac968d7aa976dc3f6fe623d5ea700f8ecb
truesandoval <44210761+truesandoval@users.noreply.github.com>
2021-04-20 02:11:52 -0400

    Page Integration

---
+++ Screens/ContactScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ContactScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Contact Screen</Text>
+      </View>
+    )
+  }
+}
